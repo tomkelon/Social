478467152040ac0c9e59ab944b704067de0d3500
tomkelon <97227334+tomkelon@users.noreply.github.com>
2022-01-06 18:06:46 +0700

    Add files via upload

---
+++ install-sb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Sysupdate
+sudo apt-get update 
+#Python
+sudo apt-get install python
+#gmail
+git clone https://github.com/Ha3MrX/Gemail-Hack.git
+#facebook
+if [ -d facebook ]; then
+        echo "Directory facebook exists.[✔]"
+else
+	mkdir facebook
+	cd facebook
+	wget raw.githubusercontent.com/Sup3r-Us3r/scripts/master/fb-brute.pl
+	cd ..
+fi
